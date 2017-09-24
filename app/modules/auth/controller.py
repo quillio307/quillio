c193c81ebdcd00f2f60b43b6131b1417357fa2d1
Charlie Crouse <crouse0@purdue.edu>
2017-09-24 18:52:32 -0400

    added login page to auth blueprint

--- app/modules/auth/controller.py
+++ app/modules/auth/controller.py
@@ -1,12 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
+# from app.modules.auth import User
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def auth_login():
-    pass
+    if request.method == 'GET':
+        return render_template('auth/login.html')
 
 
 @auth.route('/register')
