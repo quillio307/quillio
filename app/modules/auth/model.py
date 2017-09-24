623d9b26747afe1c00c256cf4e556874d8993d71
Jenna Ellis <ellis93@purdue.edu>
2017-09-24 19:05:29 -0400

    create user model

--- app/modules/auth/model.py
+++ app/modules/auth/model.py
@@ -1,1 +1,16 @@
 # HERE IS WHERE WE DEFINE USER
+from mongoengine import *
+
+connect('pyroute_db')
+
+class User(Document):
+    name = StringField(required=True, min_length=2, max_length=50)
+    email = EmailField(required=True, min_length=5, unique=True)
+    username = StringField(required=True, min_length=3, max_length=20)
+    password = StringField(required=True, min_length=6, max_length=35) #regex = none
+
+    #incomplete, finish later 
+    #def add_user(user)
+      #  if 
+
+    
