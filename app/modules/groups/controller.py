4d7814fd21cca7c492da491ee8319855ae167ed7
Ammar Husain <ammo700@gmail.com>
2017-09-29 03:56:06 -0400

    Create group route correctly assigns admin and validations on members and admin

--- app/modules/groups/controller.py
+++ app/modules/groups/controller.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, abort, request
 from flask_security import Security, login_required
+from app.modules.auth.model import User
 from app.modules.groups.model import Group
 from flask_login import current_user
-from flask import request
 import json
 from app.setup import login_manager, db
 
@@ -12,13 +12,35 @@ groups = Blueprint('groups', __name__)
 @groups.route('/create', methods=['POST'])
 @login_required
 def create_group():
+    usr = current_user._get_current_object()
     req = request.get_json(silent=True)
-    grp = Group(name=req['name'], members=[current_user._get_current_object()], admins=[current_user._get_current_object()])
-    grp.save()
-    res = {'user': current_user.email, 'req': req}
-    return json.dumps(res)
 
-'''
-@groups.route('/', methods=['POST'])
-def createGroup():
-    return ""'''
+    if not('name' in req and 'members' in req and 'admins' in req):
+        abort(400)
+
+    members = []
+
+    for email in req['members']:
+        query = User.objects(email=email)
+        if len(query) == 0:
+            req.members.remove(email)
+        else:
+            members.append(query[0])
+
+    if usr.email not in req['members']:
+        members.append(usr)
+
+    admins = []
+
+    for admin_email in req['admins']:
+        for mem in members:
+            if mem.email == admin_email:
+                admins.append(mem)
+
+    if usr.email not in req['admins']:
+        admins.append(usr)
+
+    grp = Group(name=req['name'], members=members, admins=admins)
+    grp.save()
+    res = {'message': "Success"}
+    return json.dumps(res)
