68616a0a6c3d028fd12dc5e8566c7cfd554cba08
Charlie Crouse <crouse0@purdue.edu>
2017-10-03 23:48:02 -0400

    got new meeting to succesfully create meetings

--- app/modules/meeting/model.py
+++ app/modules/meeting/model.py
@@ -1,7 +1,6 @@
-from mongoengine import *
 from flask_security import MongoEngineUserDatastore
 from wtforms import Form, validators
-from wtforms import StringField, PasswordField
+from wtforms import StringField
 
 from app.setup import db
 from app.modules.auth.model import User
@@ -10,13 +9,11 @@ from app.modules.note.model import Note
 
 class Meeting(db.Document):
     name = db.StringField(required=True, min_length=3, max_length=50)
-    owner_id = db.ReferenceField(User)
     members = db.ListField(db.ReferenceField(User))
-    meeting_data = db.ReferenceField(Note)
+    active = db.BooleanField()
 
 
 class MeetingForm(Form):
     name = StringField('Meeting Name', [validators.Length(min=3, max=50),
                                         validators.DataRequired()])
-    members = ListField(EmailField('Email Addresses',
-                                   [validators.DataRequired()]))
+    emails = StringField('Emails', [validators.DataRequired()])
