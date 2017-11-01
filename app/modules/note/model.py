3a797c2ad7941d4c7b3e0cbc05892c7dd56710df
Ammar Husain <ammo700@gmail.com>
2017-11-01 17:38:21 -0400

    Changed model for adding transcripts in array.

--- app/modules/note/model.py
+++ app/modules/note/model.py
@@ -1,10 +1,16 @@
 from app import db
+from app.modules.auth.model import User
 
-from flask_security import MongoEngineUserDatastore
-from wtforms import Form, validators, StringField, PasswordField
+
+class Transcription(db.Document):
+    user = db.ReferenceField(User)
+    transcription = db.StringField()
 
 
 class Note(db.Document):
-    transcript = db.StringField()
+    transcript = db.ListField(db.ReferenceField(Transcription), default=[])
     summary = db.StringField()
     recording = db.StringField()
+
+    def add_transcription(self, user, transcription):
+        self.transcript.append(Transcription(user, transcription))
