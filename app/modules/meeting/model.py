4176c8712ef229947768dca8fd7a9205000e5e49
Ammar Husain <ammo700@gmail.com>
2017-11-01 17:51:54 -0400

    Meeting status

--- app/modules/meeting/model.py
+++ app/modules/meeting/model.py
@@ -2,6 +2,7 @@ from app import db
 from datetime import datetime as dt
 
 from app.modules.auth.model import User
+from app.modules.note.model import Note
 
 from flask_security import MongoEngineUserDatastore
 from wtforms import Form, validators, StringField
@@ -14,6 +15,7 @@ class Meeting(db.Document):
     active = db.BooleanField()
     created_at = db.DateTimeField(default=dt.now())
     created_at_str = db.StringField(default=dt.now().strftime('%m-%d-%Y'))
+    note = db.ReferenceField(Note)
     meta = {'strict': False}
 
     def is_in_meeting(self, user):
@@ -21,6 +23,14 @@ class Meeting(db.Document):
         if user in self.members:
             return True
         return False
+
+    def status(self):
+        if self.active:
+            return 1  # Active
+        if not self.note.transcript:
+            return 0  # Not started
+        else:
+            return 2  # Completed
 
 
 class MeetingCreateForm(Form):
