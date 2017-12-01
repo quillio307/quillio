2b6753be211973babf49a8b430347f29f3276bbc
Charlie Crouse <crouse0@purdue.edu>
2017-12-01 11:59:54 -0500

    added user stats

--- app/modules/auth/model.py
+++ app/modules/auth/model.py
@@ -54,6 +54,47 @@ class User(db.Document, UserMixin):
         """ Fetches the unicode id for the User """
         return str(User.objects.get(email=self['email']).id)
 
+    def get_stats(self):
+        """ Determines the members who have the most meetings with the current
+        user """
+        friends = dict()
+        topics = dict()
+
+        for meeting in self.meetings:
+            for member in meeting.members:
+                if member == self:
+                    pass
+                elif member.name in friends:
+                    friends[member.name] = friends[member.name] + 1
+                else:
+                    friends[member.name] = 1
+
+            for topic in meeting.topics:
+                if topic in topics:
+                    topics[topic] = topics[topic] + 1
+                else:
+                    topics[topic] = 1
+        
+        sorted_friends = sorted(friends, key=friends.get, reverse=True)
+        sorted_topics = sorted(topics, key=topics.get, reverse=True)
+
+        best_friends = []
+        favorite_topics = []
+        for i in range(0, 3):
+            best_friends.append({
+                "name": sorted_friends[i],
+                "count": friends[sorted_friends[i]]
+            })
+            favorite_topics.append({
+                "name": sorted_topics[i],
+                "count": topics[sorted_topics[i]]
+            })
+        
+        result = dict()
+        result['members'] = best_friends 
+        result['topics'] = favorite_topics 
+        return result
+
 
 class SignupForm(Form):
     email = StringField('Email', [validators.DataRequired(),
